88681ea17f1e448ce3ffdba1f0a2067290c9fe4b
Karl Stroetmann <karl.stroetmann@dhbw-mannheim.de>
2021-02-18 16:22:41 +0100

    constraint propagation improved

--- Python/ai.sh
+++ Python/ai.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-conda create -y -n ai python=3.7 jupyter notebook
+conda create -y -n ai python=3 jupyter notebook
 source activate ai
-conda install -y "nbconvert=5.6.1"
-conda install -y -c conda-forge jupyter_contrib_nbextensions
 conda install -y -c anaconda graphviz
 conda install -y -c conda-forge python-graphviz
 conda install -y -c anaconda ply
 conda install -y -c conda-forge ipycanvas
-pip install jedi==0.17.2
-pip install python-chess
+pip install chess
+
