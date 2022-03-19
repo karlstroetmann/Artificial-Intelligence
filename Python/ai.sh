27b94215c65cf127b4063dd2803d3eaf1f823b2c
karlstroetmann <karl.stroetmann@dhbw-mannheim.de>
2022-03-20 00:15:47 +0100

    run latex

--- Python/ai.sh
+++ Python/ai.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 conda create -y -n ai python=3 jupyter notebook
 conda activate ai
-conda install -c anaconda -y graphviz ply seaborn scikit-learn
+conda install -c anaconda -y graphviz ply seaborn scikit-learn keras tensorflow
 conda install -c conda-forge -y python-graphviz ipycanvas matplotlib memory_profiler
 pip install chess 
 
