584ad82f19d9c21e2fb8272ffcc1b9fff7371187
Karl Stroetmann <karl.stroetmann@dhbw-mannheim.de>
2022-06-15 18:34:54 +0200

    minor mistakes corrected

--- Python/ai.sh
+++ Python/ai.sh
@@ -4,4 +4,5 @@ conda activate ai
 conda install -c anaconda -y graphviz ply seaborn scikit-learn 
 conda install -c conda-forge -y python-graphviz ipycanvas matplotlib memory_profiler autograd jupyter notebook
 pip install chess 
+pip install nbimporter
 
