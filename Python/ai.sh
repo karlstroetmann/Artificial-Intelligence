4f7df61539622762ca199847122d4fbac4409d3e
Karl Stroetmann <karl.stroetmann@dhbw-mannheim.de>
2023-02-22 18:12:04 +0100

    Nim-Frame added

--- Python/ai.sh
+++ Python/ai.sh
@@ -2,7 +2,8 @@
 conda create -y -n ai python=3.10 
 conda activate ai
 conda install -c anaconda -y graphviz ply seaborn scikit-learn 
-conda install -c conda-forge -y python-graphviz ipycanvas matplotlib memory_profiler autograd jupyter notebook
+conda install -c conda-forge -y python-graphviz matplotlib memory_profiler autograd jupyter notebook
+pip install ipycanvas
 pip install chess 
 pip install nbimporter
 pip install z3-solver
