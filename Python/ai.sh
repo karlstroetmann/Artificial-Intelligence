73d5652dc7ea1c1ab04136f077f47a0a9c1ee5e2
Karl Stroetmann <karl.stroetmann@dhbw-mannheim.de>
2024-01-11 16:46:19 +0100

    into updated

--- Python/ai.sh
+++ Python/ai.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-conda create -y -n ai python=3.10 
+conda create -y -n ai python=3.12 
 conda activate ai
 conda install -c anaconda -y graphviz ply seaborn scikit-learn 
-conda install -c conda-forge -y python-graphviz matplotlib memory_profiler autograd jupyter notebook
+conda install -c conda-forge -y jupyter python-graphviz matplotlib memory_profiler autograd 
 pip install ipycanvas
 pip install chess 
-pip install nbimporter
 pip install z3-solver
