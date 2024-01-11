0d9da7bfc06267e7d0552fb016aa77bd15e839ac
Karl Stroetmann <karl.stroetmann@dhbw-mannheim.de>
2024-01-11 18:39:40 +0100

    added type information

--- Python/ai.sh
+++ Python/ai.sh
@@ -3,6 +3,7 @@ conda create -y -n ai python=3.12
 conda activate ai
 conda install -c anaconda -y graphviz ply seaborn scikit-learn 
 conda install -c conda-forge -y jupyter python-graphviz matplotlib memory_profiler autograd 
+pip install nb_mypy
 pip install ipycanvas
 pip install chess 
 pip install z3-solver
