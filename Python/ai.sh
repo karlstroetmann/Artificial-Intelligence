70656a143616160288fb95e33c47328172f74453
Nereuxofficial <37740907+Nereuxofficial@users.noreply.github.com>
2024-01-19 16:34:13 +0100

    feat: complete pip commands

--- Python/ai.sh
+++ Python/ai.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 conda create -y -n ai python=3.11 jupyter
 conda activate ai
 conda install -c anaconda -y nbclassic graphviz ply seaborn scikit-learn 
 conda install -c conda-forge -y python-graphviz matplotlib memory_profiler autograd ipycanvas 
-python3 -m pip install nb_mypy
-python3 -m pip install chess 
-pip install z3-solver
+./venv.sh
