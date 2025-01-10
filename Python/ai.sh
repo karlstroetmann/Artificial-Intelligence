58a17f995cc7f3769c61b252b8e344888c4c5fc1
Karl Stroetmann <karl.stroetmann@dhbw-mannheim.de>
2025-01-10 17:18:27 +0100

    restructuring, remove data mining

--- Python/ai.sh
+++ Python/ai.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
-conda create -y -n ai python=3.11 jupyter
+conda create -y -n ai python=3.12
 conda activate ai
-conda install -c anaconda -y nbclassic graphviz ply seaborn scikit-learn 
-conda install -c conda-forge -y python-graphviz matplotlib memory_profiler autograd ipycanvas 
-./venv.sh
+pip install --upgrade pip
+pip install notebook jupyterlab
+pip install nbclassic
+conda install -c anaconda -y graphviz ply seaborn scikit-learn 
+conda install -c conda-forge -y python-graphviz matplotlib ipycanvas 
+pip install nb_mypy
+pip install z3-solver
+pip install git+https://github.com/reclinarka/problem_visuals
+pip install git+https://github.com/reclinarka/chess-problem-visuals
+# start notebook with jupyter nbclassic
