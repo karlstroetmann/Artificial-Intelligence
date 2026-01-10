b206b49bd69df9e53d08e35b893d6d8c50bfef5f
Karl Stroetmann <karl.stroetmann@dhbw-mannheim.de>
2026-01-10 13:58:25 +0100

    minor changes

--- Python/ai.sh
+++ Python/ai.sh
@@ -1,14 +1,13 @@
-#!/bin/sh
-conda create -y -n ai python=3.12
+conda create -n ai
 conda activate ai
-pip install --upgrade pip
-pip install notebook jupyterlab
-pip install nbclassic
-conda install -c anaconda -y graphviz ply seaborn scikit-learn 
-conda install -c conda-forge -y python-graphviz matplotlib ipycanvas 
-pip install nb_mypy
-pip install z3-solver
-pip install git+https://github.com/reclinarka/problem_visuals
-pip install git+https://github.com/reclinarka/chess-problem-visuals
-pip install memory_profiler
-# start notebook with jupyter nbclassic
+conda install -y -c conda-forge python=3.14
+conda install -y -c conda-forge jupyter nbclassic
+pip install ply
+conda install -y -c conda-forge graphviz
+conda install -y -c conda-forge python-graphviz
+pip install mypy
+pip install nb-mypy
+conda install -y -c conda-forge numpy matplotlib seaborn
+
+
+
